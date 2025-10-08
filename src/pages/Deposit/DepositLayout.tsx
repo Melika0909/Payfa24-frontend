e29126811b435b2095637b888928c987398b06dc
Melika-shybani <melikashybani6@gmail.com>
2025-10-08 12:22:55 +0330

    a

--- src/pages/Deposit/DepositLayout.tsx
+++ src/pages/Deposit/DepositLayout.tsx
@@ -21,7 +21,7 @@ const DepositLayout: React.FC<DepositLayoutProps> = ({
   return (
     <div className="w-full container-style h-full flex flex-col lg:flex-row items-center justify-center">
       {/* بخش اصلی (راست و چپ) */}
-      <div className=" flex ششش w-full lg:gap-7 lg:flex-row-reverse lg:p-10 flex-col items-start mt-14 justify-center lg:shadow-[0_0_12px_0_rgba(0,0,0,0.16)] rounded-2xl">
+      <div className=" flex شhشش w-full lg:gap-7 lg:flex-row-reverse lg:p-10 flex-col items-start mt-14 justify-center lg:shadow-[0_0_12px_0_rgba(0,0,0,0.16)] rounded-2xl">
         
         {/*
           بخش راست - ثابت (ستون انتخاب‌های واریز)
